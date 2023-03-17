24afc1970b1a55681196bab32d1dd65378118bc3
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2023-03-17 13:02:09 -0700

    implemented generateFunctionFromTests.ts -- not yet tested

--- src/copilotIntegration/generateFunctionFromTests.ts
+++ src/copilotIntegration/generateFunctionFromTests.ts
@@ -1,37 +1,63 @@
-// THIS IS PAST CODE FROM EXTENSION.JS
-// const input = await vscode.window.showInputBox({
-// 				prompt: "Ask code-davinci-003 a question",
-// 				placeHolder: "put question here",
-// 			});
+import * as vscode from "vscode";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
 
-// 			if (input) {
-// 				// configuring openai -------------------------------------------------- test
-// 				dotenv.config({ path: "C:\\Users\\vbmat\\Projects\\testwise\\.env" });
+// --------------------- temp config ----------------------------------
+dotenv.config({ path: "C:\\Users\\vbmat\\Projects\\testwise\\.env" });
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const modelName = "text-davinci-003";
+const temperature = 0.2;
+const maxTokens = 10;
+// ------------------- temp config (end) ---------------------------------
 
-// 				const configuration = new Configuration({
-// 					apiKey: process.env.OPENAI_API_KEY,
-// 				});
-// 				const openai = new OpenAIApi(configuration);
-// 				const modelName = "text-davinci-003";
-// 				const response = await openai.createCompletion({
-// 					model: modelName,
-// 					prompt: input,
-// 					temperature: 0.2,
-// 					max_tokens: 10,
-// 				});
-// // configuring openai -------------------------------------------------- test
-// if (response && response.status === 200) {
-//     console.log(response); //.data.choices[0].text)
-//     vscode.window.showInformationMessage(
-//         `answer: ${response.data.choices[0].text}`
-//     );
-// } else {
-//     console.log(`error: ${response.statusText}`);
-// }
+async function fetchFunctionFromOpenAI(input: string): Promise<string | null> {
+	try {
+		if (input) {
+			dotenv.config({ path: "C:\\Users\\vbmat\\Projects\\testwise\\.env" }); // TODO: remove when 'settings' is implemented
 
-function generatePrompt(input: string) {
-	// very inefficient, just for testing
-	return `Answer this: ${input}`;
+			const response = await openai.createCompletion({
+				model: modelName,
+				prompt: input,
+				temperature: temperature,
+				max_tokens: maxTokens,
+			});
+
+			if (response && response.status === 200) {
+				console.log(response); // TODO: remove (debugging)
+				return response.data.choices[0].text!; // TODO: ! is not safe
+			} else {
+				console.log(`error: ${response.statusText}`);
+				return null;
+			}
+		} else {
+			return null;
+		}
+	} catch (error) {
+		console.error(`Error fetching function from response:`, error);
+		return null;
+	}
 }
 
-async function configureOpenAI() {}
+export async function generateFunctionFromTests(
+	testCode: string
+): Promise<string | null> {
+	if (!process.env.apiKey) {
+		vscode.window.showErrorMessage(
+			"Please set the API key in TestWise settings."
+		);
+		return null;
+	}
+
+	const prompt = generatePrompt(testCode);
+
+	const generatedFunction = await fetchFunctionFromOpenAI(prompt);
+	return generatedFunction;
+}
+
+function generatePrompt(input: string) {
+	// TODO: replace with proper prompting file
+	return `Generate a function that passes the following test suite:\n\n${input}\n\nFunction:`;
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import * as dotenv from "dotenv";
-import { Configuration, OpenAIApi } from "openai";
-import { generateFunctionFromTests } from "./copilotIntegration";
+import { generateFunctionFromTests } from "./copilotIntegration/generateFunctionFromTests";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
