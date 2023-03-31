6c255ce3d04d441d093ce1b50007584d50d99264
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2023-03-31 03:25:42 -0700

    fixed bug in hasValidAPIKey -- hadnt entered the config name yet

--- src/helpers/hasValidAPIKey.ts
+++ src/helpers/hasValidAPIKey.ts
@@ -1,9 +1,7 @@
 import * as vscode from "vscode";
 
 export function hasValidAPIKey(): boolean {
-	const apiKey = vscode.workspace
-		.getConfiguration("yourExtensionName")
-		.get("apiKey");
+	const apiKey = vscode.workspace.getConfiguration("testwise").get("apiKey");
 	return (
 		apiKey !== undefined && apiKey !== null && apiKey.toString().trim() !== ""
 	);
