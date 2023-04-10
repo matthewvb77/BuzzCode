29131df660f1450f52a871500b6b9aae14cd84d7
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2023-04-10 13:33:04 -0700

    adding into prompt to use something like selenium for web interface interaction

--- src/AIContainer/prompts.ts
+++ src/AIContainer/prompts.ts
@@ -4,7 +4,7 @@ export const initializePrompt = `You have access to these functions:
 3. recurse(newPrompt: string) --> This will happen automatically if any error occurs.
 4. askUser(question: string) --> Ask the user a question and recurse with their response.
 
-Minimize user intervention. Don't use askUser unless you have to.
+Minimize user intervention. Don't use askUser unless you have to. If you need to interact with web interfaces, use a tool like Selenium WebDriver.
 
 Example of an instruction list:
 
@@ -12,7 +12,7 @@ Example of an instruction list:
 
 `;
 
-export const taskPrompt = `Generate a JSON instruction list using the commands to solve the following prompt. Escape necessary characters with backslashes:\n\n`;
+export const taskPrompt = `Generate a JSON instruction list using the commands to solve the following prompt. Escape necessary characters with backslashes: `;
 
 /*
 {
