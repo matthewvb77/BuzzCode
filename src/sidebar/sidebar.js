b3102d41e6fa590fc8f910a20580e55b97b690a0
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2023-04-10 18:24:32 -0700

    made it so the response label&box are visible on question input mode only

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,26 +1,32 @@
 (function () {
-	const userInput = document.getElementById("user-input");
+	const userInputBox = document.getElementById("user-input");
 	const inputTypeSelect = document.getElementById("input-type");
+	const responseLabel = document.getElementById("response-label");
+	const responseArea = document.getElementById("response-area");
 
-	function updatePlaceholder() {
+	function updatePlaceholderAndResponse() {
 		switch (inputTypeSelect.value) {
 			case "task":
-				userInput.placeholder = "Give a task...";
+				userInputBox.placeholder = "Give a task...";
+				responseLabel.style.display = "none";
+				responseArea.style.display = "none";
 				break;
 			case "question":
-				userInput.placeholder = "Ask a question...";
+				userInputBox.placeholder = "Ask a question...";
+				responseLabel.style.display = "block";
+				responseArea.style.display = "block";
 				break;
 			default:
 				throw new Error("Invalid input type");
 		}
 	}
 
-	updatePlaceholder();
-	inputTypeSelect.addEventListener("change", updatePlaceholder);
+	updatePlaceholderAndResponse();
+	inputTypeSelect.addEventListener("change", updatePlaceholderAndResponse);
 
 	document.getElementById("submit-button").addEventListener("click", () => {
-		const input = document.getElementById("user-input").value;
-		const inputType = document.getElementById("input-type").value;
+		const input = userInputBox.value;
+		const inputType = inputTypeSelect.value;
 
 		vscode.postMessage({ command: "submit", input, inputType });
 	});
