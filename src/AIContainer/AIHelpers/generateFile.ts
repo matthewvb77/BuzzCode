4362317503b3f00e520f3b8a6658999b5b99bd63
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2023-04-15 14:40:23 -0700

    added warning message if you're about to overwrite a file

--- src/AIContainer/AIHelpers/generateFile.ts
+++ src/AIContainer/AIHelpers/generateFile.ts
@@ -22,5 +22,27 @@ export async function generateFile(
 
 	const newFileUri = vscode.Uri.joinPath(workspaceFolder, fileName);
 
+	// Check if file exists
+	try {
+		await vscode.workspace.fs.stat(newFileUri);
+		// If stat is successful, the file exists
+		const overwrite = await vscode.window.showWarningMessage(
+			`File '${fileName}' already exists. Do you want to overwrite it?`,
+			{ modal: true },
+			"Yes"
+		);
+
+		if (overwrite !== "Yes") {
+			// User chose not to overwrite the file
+			return;
+		}
+	} catch (error) {
+		// An error occurred other than FileNotFound
+		vscode.window.showErrorMessage(
+			`An error occurred: ${(error as Error).message}`
+		);
+		return;
+	}
+
 	await vscode.workspace.fs.writeFile(newFileUri, Buffer.from(contents));
 }
