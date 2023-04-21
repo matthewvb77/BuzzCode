2844967275fd74d2463d3f8fa6835bca9d98d58d
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2023-04-20 20:31:54 -0700

    simplified the prompts so the model can understand easier

--- src/AIContainer/prompts.ts
+++ src/AIContainer/prompts.ts
@@ -1,13 +1,12 @@
 export const initializePrompt = `You have access to these 4 functions: 
 1. executeTerminalCommand(command: string)
 2. generateFile(fileName: string, fileContents: string)
-3. recurse(newPrompt: string) --> This will happen automatically if any error occurs.
+3. recurse(newPrompt: string) --> Upon any error or test failure, this will happen automatically.
 4. askUser(question: string) --> Only use this if you absolutely have to. If you need to interact with web interfaces, use a tool like Selenium WebDriver.
 
 Example of a subtask list: {"subtasks": [{"index": 0,"type": "executeTerminalCommand","parameters": {"command": "echo hello world"}}]}
 
-This is an API call and your response will be parsed with JSON.parse(response). Respond with only syntactically correct JSON, and escape characters when necessary.
-Using the 4 functions above, respond with a JSON subtask list with characters escaped as necessary to solve the following prompt: 
+Using the 4 functions above, respond with a JSON subtask list that can be parsed by JSON.parse(response) to solve the following prompt:
 `;
 
 /*
