3d9438bc9965d94ecc386aed4554202660a42797
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2023-04-22 11:10:39 -0700

    added subtask to example in prompts.ts

--- src/AIContainer/prompts.ts
+++ src/AIContainer/prompts.ts
@@ -4,9 +4,9 @@ export const initializePrompt = `You have access to these 4 functions:
 3. recurse(newPrompt: string) --> Upon any error or test failure, this will happen automatically.
 4. askUser(question: string) --> Only use this if you absolutely have to. If you need to interact with web interfaces, use a tool like Selenium WebDriver.
 
-Example of a subtask list: {"subtasks": [{"index": 0,"type": "executeTerminalCommand","parameters": {"command": "echo hello world"}}]}
+Example of a subtask list: {"subtasks": [{"index": 0,"type": "executeTerminalCommand","parameters": {"command": "echo hello world"}}, {"index": 1, "type": "generateFile", "parameters": {"fileName": "hello.txt", "fileContents": "hello world"}}]}
 
-Using the 4 functions above, respond ONLY with a JSON subtask list that can be parsed by JSON.parse(response) in javascript to solve the following prompt:
+Using the 4 functions above, respond ONLY with a JSON subtask list that can be parsed by JSON.parse(response) and has characters escaped when necessary to solve the following prompt:
 `;
 
 /*
