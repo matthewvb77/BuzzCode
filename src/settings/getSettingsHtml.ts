2674749ecd46ddfd1f0c1dc9f35a35a09b7a3904
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2023-04-28 10:59:20 -0700

    enabling gpt-4 option

--- src/settings/getSettingsHtml.ts
+++ src/settings/getSettingsHtml.ts
@@ -49,7 +49,7 @@ export function getSettingsHtml(
                     </div>
                     <label for="model">Model:</label>
                     <select id="model" name="model">
-                        <option value="gpt-4" disabled="true">GPT-4 - Not yet supported</option>
+                        <option value="gpt-4">GPT-4</option>
                         <option value="gpt-3.5-turbo" ${
 													model === "gpt-3.5-turbo" ? "selected" : ""
 												}>GPT-3.5-Turbo</option>
