20e93eadad16910d2ed0713579c0cbc3d3f51c0c
Matthew Van Brummelen <vb.matthew@gmail.com>
2023-05-25 14:28:04 -0700

    converting jsonFixGeneral test to use mocha, also installed chai for mocha

--- src/test/suite/jsonFixGeneral.test.ts
+++ src/test/suite/jsonFixGeneral.test.ts
@@ -1,27 +1,28 @@
+import { expect } from "chai";
 import { correctJson } from "../../helpers/jsonFixGeneral";
 
 describe("JSON Correction", () => {
 	it("should not modify valid JSON", () => {
 		const validJson = '{"name":"John", "age":30, "city":"New York"}';
 		const result = correctJson(validJson);
-		expect(result).toBe(validJson);
+		expect(result).to.equal(validJson);
 	});
 	it("should fix bad escaped characters", () => {
 		const invalidJson = `{"name":"Jo\\hn"}`;
 		const expectedJson = `{"name":"John"}`;
 		const result = correctJson(invalidJson);
-		expect(result).toBe(expectedJson);
+		expect(result).to.equal(expectedJson);
 	});
 	it("should balance braces", () => {
 		const unbalancedJson = '{"name":"John", "age":30, "city":"New York"';
 		const expectedJson = '{"name":"John", "age":30, "city":"New York"}';
 		const result = correctJson(unbalancedJson);
-		expect(result).toBe(expectedJson);
+		expect(result).to.equal(expectedJson);
 	});
 	it("should add quotes to property names", () => {
 		const invalidJson = '{name:"John", age:30, city:"New York"}';
 		const expectedJson = '{"name":"John", "age":30, "city":"New York"}';
 		const result = correctJson(invalidJson);
-		expect(result).toBe(expectedJson);
+		expect(result).to.equal(expectedJson);
 	});
 });
