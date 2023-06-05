10e2de5cbc60071c7a92acb2f1b460871d821be9
Matthew Van Brummelen <vb.matthew@gmail.com>
2023-06-05 10:56:16 -0700

    minor prompt change a initialization of recusiveDev test suite

--- src/AIContainer/prompts.ts
+++ src/AIContainer/prompts.ts
@@ -1,6 +1,6 @@
 import { shell } from "../settings/configuration";
 
-export const initializePrompt = `Your environment is ${process.platform} and you have access to these 3 functions: 
+export const initializePrompt = `Your environment is ${process.platform} and you have access to these functions: 
 - executeTerminalCommand(command: string) --> shell type is ${shell}. output is saved and included in recurse() call. The user can see the output.
 - generateFile(fileName: string, fileContents: string) --> always use this when creating files.
 - askUser(question: string) --> response is saved and included in recurse() call.
@@ -10,7 +10,7 @@ Always use terminal commands and recurse to gain information and context.
 
 Response example: {"subtasks": [{ "index": 0, "type": "executeTerminalCommand", "parameters": { "command": "echo hello world"}}]}
 
-Using the 3 functions above, respond with a JSON subtask list to solve the following prompt:
+Using the functions above, respond with a JSON subtask list to solve the following prompt:
 `;
 
 /*
---
+++ src/test/suite/recursiveDevelopment.test.ts
@@ -0,0 +1,39 @@
+import * as vscode from "vscode";
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+import * as sinon from "sinon";
+import * as nock from "nock";
+import { recursiveDevelopment } from "../../AIContainer/recursiveDevelopment";
+import { TerminalObject } from "../../objects/terminalObject";
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe("recursiveDevelopment function", function () {
+	let sandbox: sinon.SinonSandbox;
+
+	beforeEach(function () {
+		sandbox = sinon.createSandbox();
+	});
+
+	afterEach(function () {
+		sandbox.restore();
+	});
+
+	it("should resolve with a string if there is an error during terminal creation", async function () {
+		const signal: AbortSignal = new AbortController().signal;
+		sandbox.stub(TerminalObject, "create").throws(new Error("Test error"));
+
+		const result = await recursiveDevelopment(
+			"",
+			signal,
+			sinon.fake(),
+			sinon.fake(),
+			sinon.fake()
+		);
+
+		expect(result).to.equal("Error: Test error");
+	});
+
+	// Add more tests for the different branches of your code here...
+});
