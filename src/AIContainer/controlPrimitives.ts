64fc78afccc8bdfc36c87fd877a11bd01bb0af1f
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2023-09-14 17:47:33 -0700

    fixing functionCaller format

--- src/AIContainer/controlPrimitives.ts
+++ src/AIContainer/controlPrimitives.ts
@@ -1,68 +1,5 @@
 import { shell } from "../settings/configuration";
 
-const controlPrimitives = [
-	{
-		name: "executeTerminalCommand",
-		description: `Execute a terminal command in the ${shell} shell`,
-		parameters: {
-			type: "object",
-			properties: {
-				command: {
-					type: "string",
-					description: "The command to execute",
-				},
-			},
-			required: ["command"],
-		},
-	},
-	{
-		name: "generateFile",
-		description: "Generate a file with the given name and contents",
-		parameters: {
-			type: "object",
-			properties: {
-				fileName: {
-					type: "string",
-					description: "The name of the file to generate",
-				},
-				fileContents: {
-					type: "string",
-					description: "The contents of the file to generate",
-				},
-			},
-			required: ["fileName", "fileContents"],
-		},
-	},
-	{
-		name: "askUser",
-		description: "Ask the user a question -- only use as a last resort",
-		parameters: {
-			type: "object",
-			properties: {
-				question: {
-					type: "string",
-					description: "The question to ask the user",
-				},
-			},
-			required: ["question"],
-		},
-	},
-	{
-		name: "recurse",
-		description: "Recurse with the given prompt",
-		parameters: {
-			type: "object",
-			properties: {
-				newPrompt: {
-					type: "string",
-					description: "The prompt to recurse with",
-				},
-			},
-			required: ["newPrompt"],
-		},
-	},
-];
-
 /*
 OpenAI's function calling api is limited to returning a single function call.
 This function allows us to call multiple functions in sequence.
@@ -74,7 +11,69 @@ export const functionCaller = [
 		parameters: {
 			type: "object",
 			properties: {
-				controlPrimitives,
+				executeTerminalCommand: {
+					name: "executeTerminalCommand",
+					description: `Execute a terminal command in the ${shell} shell`,
+					parameters: {
+						type: "object",
+						properties: {
+							command: {
+								type: "string",
+								description: "The command to execute",
+							},
+						},
+						required: ["command"],
+					},
+				},
+
+				generateFile: {
+					name: "generateFile",
+					description: "Generate a file with the given name and contents",
+					parameters: {
+						type: "object",
+						properties: {
+							fileName: {
+								type: "string",
+								description: "The name of the file to generate",
+							},
+							fileContents: {
+								type: "string",
+								description: "The contents of the file to generate",
+							},
+						},
+						required: ["fileName", "fileContents"],
+					},
+				},
+
+				askUser: {
+					name: "askUser",
+					description: "Ask the user a question -- only use as a last resort",
+					parameters: {
+						type: "object",
+						properties: {
+							question: {
+								type: "string",
+								description: "The question to ask the user",
+							},
+						},
+						required: ["question"],
+					},
+				},
+
+				recurse: {
+					name: "recurse",
+					description: "Recurse with the given prompt",
+					parameters: {
+						type: "object",
+						properties: {
+							newPrompt: {
+								type: "string",
+								description: "The prompt to recurse with",
+							},
+						},
+						required: ["newPrompt"],
+					},
+				},
 			},
 			required: [],
 		},
