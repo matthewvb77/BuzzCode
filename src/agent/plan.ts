3ba8924d9a63f92261946a510d793d3648a415de
Matthew Van Brummelen <vb.matthew@gmail.com>
2023-09-20 23:44:53 -0700

    wrote high-level plan code

--- src/agent/plan.ts
+++ src/agent/plan.ts
@@ -4,6 +4,7 @@ import { initializePrompt, highLevelPlanningPrompt } from "./prompts";
 import { queryChatGPT } from "../helpers/queryChatGPT";
 import { correctJson } from "../helpers/jsonFixGeneral";
 import { RETURN_CANCELLED, ERROR_PREFIX } from "../settings/configuration";
+import { askUser } from "./askUser";
 
 /*
     INPUT: The user's task.
@@ -19,6 +20,52 @@ export async function plan(
 		highLevelPlanningPrompt + task + "\n\nJSON question list:",
 		signal
 	);
+
+	if (responseQuestions.startsWith("[") && responseQuestions.endsWith("]")) {
+		responseString = `{"questions": ${responseQuestions}}`;
+	}
+
+	if (responseQuestions === RETURN_CANCELLED) {
+		return RETURN_CANCELLED;
+	} else if (responseQuestions.startsWith("Error")) {
+		return responseQuestions;
+	}
+
+	/* -------------------------- Parse Questions -------------------------- */
+
+	try {
+		// Regular expression to match JSON
+		const jsonRegex = /{[\s\S]*}/;
+
+		// Extract JSON and reasoning strings
+		var jsonQuestionsStringArray: Array<string> | null = null;
+		try {
+			jsonQuestionsStringArray = responseQuestions.match(jsonRegex);
+		} catch (error) {
+			jsonQuestionsStringArray =
+				correctJson(responseQuestions).match(jsonRegex);
+		}
+		if (!jsonQuestionsStringArray) {
+			throw new Error("No JSON found.");
+		}
+		let questionsString = jsonQuestionsStringArray[0];
+
+		questionsString = correctJson(questionsString);
+
+		var questions: Array<string> = JSON.parse(questionsString).questions;
+		var askUserResponse: string = "";
+
+		// NOTE FOR TOMORROW: THIS WHOLE FILE MIGHT HAVE TO GO BACK INTO RECURSIVE DEVELOPMENT. OR WE COULD PASS THE SUBTASKUPDATE FUNCTIONS AS CALLBACKS INTO THIS FUNCTION
+		questions.forEach((question) => {
+			askUserResponse +=
+				`{ Question: { ${question} }\n` +
+				`Response: { ${await askUser(question)} }\n }, \n`;
+		});
+	} catch (error) {
+		return ERROR_PREFIX + "Invalid JSON. \n" + (error as Error).message;
+	}
+
+	/* ---------------------- Subtask Planning ---------------------- */
 
 	var responseString: string = await queryChatGPT(
 		initializePrompt + task + "\n\nJSON subtask list:",
@@ -40,16 +87,16 @@ export async function plan(
 		const jsonRegex = /{[\s\S]*}/;
 
 		// Extract JSON and reasoning strings
-		var jsonStringArray: Array<string> | null = null;
+		var jsonQuestionsStringArray: Array<string> | null = null;
 		try {
-			jsonStringArray = responseString.match(jsonRegex);
+			jsonQuestionsStringArray = responseString.match(jsonRegex);
 		} catch (error) {
-			jsonStringArray = correctJson(responseString).match(jsonRegex);
+			jsonQuestionsStringArray = correctJson(responseString).match(jsonRegex);
 		}
-		if (!jsonStringArray) {
+		if (!jsonQuestionsStringArray) {
 			throw new Error("No JSON found.");
 		}
-		let jsonString = jsonStringArray[0];
+		let jsonString = jsonQuestionsStringArray[0];
 		let reasoning = responseString
 			.replace(jsonRegex, "")
 			.trim()
