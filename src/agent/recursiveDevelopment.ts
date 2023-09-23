9653d6a74769cd5ee626966b8ed5f94346571e3c
Matthew Van Brummelen <vb.matthew@gmail.com>
2023-09-23 15:32:07 -0700

    removed plan file and aggregated code into recursiveDevelopment

--- src/agent/recursiveDevelopment.ts
+++ src/agent/recursiveDevelopment.ts
@@ -1,6 +1,5 @@
 import * as vscode from "vscode";
 import { TerminalObject, CommandResult } from "./terminalObject";
-import { Subtask } from "./subtask";
 import { askUser } from "./askUser";
 import {
 	delay,
@@ -9,7 +8,10 @@ import {
 	ERROR_PREFIX,
 	RECURSION_LIMIT,
 } from "../settings/configuration";
-import { plan } from "./plan";
+import { Subtask, SubtaskState } from "./subtask";
+import { queryChatGPT } from "../helpers/queryChatGPT";
+import { correctJson } from "../helpers/correctJson";
+import { highLevelPlanningPrompt, initializePrompt } from "./prompts";
 
 var recursionCount = 0;
 var taskDescription = ``;
@@ -75,6 +77,111 @@ async function recursiveDevelopmentHelper(
 	if (recursionCount >= RECURSION_LIMIT) {
 		return ERROR_PREFIX + "Recursion limit reached";
 	}
+
+	/* ----------------------- Planning Phase ----------------------- */
+
+	var responseQuestions: string = await queryChatGPT(
+		highLevelPlanningPrompt + input + "\n\nJSON question list:",
+		signal
+	);
+
+	if (responseQuestions.startsWith("[") && responseQuestions.endsWith("]")) {
+		responseString = `{"questions": ${responseQuestions}}`;
+	}
+
+	if (responseQuestions === RETURN_CANCELLED) {
+		return RETURN_CANCELLED;
+	} else if (responseQuestions.startsWith("Error")) {
+		return responseQuestions;
+	}
+
+	try {
+		// Regular expression to match JSON
+		const jsonRegex = /{[\s\S]*}/;
+
+		// Extract JSON and reasoning strings
+		var jsonQuestionsStringArray: Array<string> | null = null;
+		try {
+			jsonQuestionsStringArray = responseQuestions.match(jsonRegex);
+		} catch (error) {
+			jsonQuestionsStringArray =
+				correctJson(responseQuestions).match(jsonRegex);
+		}
+		if (!jsonQuestionsStringArray) {
+			throw new Error("No JSON found.");
+		}
+		let questionsString = jsonQuestionsStringArray[0];
+
+		questionsString = correctJson(questionsString);
+
+		var questions: Array<string> = JSON.parse(questionsString).questions;
+		var askUserResponse: string = "";
+
+		// NOTE FOR TOMORROW: THIS WHOLE FILE MIGHT HAVE TO GO BACK INTO RECURSIVE DEVELOPMENT. OR WE COULD PASS THE SUBTASKUPDATE FUNCTIONS AS CALLBACKS INTO THIS FUNCTION
+		questions.forEach(async (question) => {
+			askUserResponse +=
+				`{ Question: { ${question} }\n` +
+				`Response: { ${await askUser(question)} }\n }, \n`;
+		});
+	} catch (error) {
+		return ERROR_PREFIX + "Invalid JSON. \n" + (error as Error).message;
+	}
+
+	/* ---------------------- Subtask Planning ---------------------- */
+
+	var responseString: string = await queryChatGPT(
+		initializePrompt + holduptemp + "\n\nJSON subtask list:",
+		signal
+	);
+
+	if (responseString.startsWith("[") && responseString.endsWith("]")) {
+		responseString = `{"subtasks": ${responseString}}`;
+	}
+
+	if (responseString === RETURN_CANCELLED) {
+		return RETURN_CANCELLED;
+	} else if (responseString.startsWith("Error")) {
+		return responseString;
+	}
+
+	try {
+		// Regular expression to match JSON
+		const jsonRegex = /{[\s\S]*}/;
+
+		// Extract JSON and reasoning strings
+		var jsonQuestionsStringArray: Array<string> | null = null;
+		try {
+			jsonQuestionsStringArray = responseString.match(jsonRegex);
+		} catch (error) {
+			jsonQuestionsStringArray = correctJson(responseString).match(jsonRegex);
+		}
+		if (!jsonQuestionsStringArray) {
+			throw new Error("No JSON found.");
+		}
+		let jsonString = jsonQuestionsStringArray[0];
+		let reasoning = responseString
+			.replace(jsonRegex, "")
+			.trim()
+			.split("Response: ")[0];
+
+		jsonString = correctJson(jsonString);
+
+		var subtasks: Array<Subtask> = JSON.parse(jsonString).subtasks;
+
+		subtasks.forEach((subtask) => {
+			subtask.state = SubtaskState.initial;
+		});
+		if (subtasks.length - 1 !== subtasks[subtasks.length - 1].index) {
+			throw new Error("Invalid subtask indices.");
+		}
+		if (reasoning) {
+			vscode.window.showInformationMessage("Reasoning:\n" + reasoning);
+		}
+	} catch (error) {
+		return ERROR_PREFIX + "Invalid JSON. \n" + (error as Error).message;
+	}
+	return subtasks;
+	/* ---------------- Execution Phase ---------------- */
 
 	var subtasks: string | Array<Subtask> = await plan(input, signal);
 	if (typeof subtasks === "string") {
